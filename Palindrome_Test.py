ed28ca18b388d1e86a5ce7581e344ccf1502aa69
Laxman Kumar <59361284+laxmankumar2000@users.noreply.github.com>
2020-05-17 16:05:49 +0530

    Update Palindrome_Test.py

--- Palindrome_Test.py
+++ Palindrome_Test.py
@@ -1,7 +1,7 @@
 '''
 
 A string is said to be palindrome if reverse of the string is same as string.
-For example, “radar” is main string and it's reverse string is 'radar',So main string is equle to reverse string. So 'radar' is palindome string.
+For example, “radar” is main string and it's reverse string is 'radar',So main string is equle to reverse string. So 'radar' is palindrome string.
 'radix' is main string and it's reverse string is'xidar'.So main string is not equle reverse string.So 'radix' is not palindrome string.
 
 '''
@@ -10,25 +10,25 @@ For example, “radar” is main string and it's reverse string is 'radar',So ma
     str1=input('enter the string')                           # string input
     str2=str1[::-1]                                          # reverse string
     if str2==str1:
-        print('string is palindome.')
+        print('string is palindrome.')
     else:
-        print('string i not palindome.')
+        print('string i not palindrome.')
 
 
 
 
 
 # 2.By using function.
-    def palindome_check(str):
+    def palindrome_check(str):
         out=str[::-1]
         return out
 
 
     str=input('enter the string')
-    out=palindome_check(str)
+    out=palindrome_check(str)
     if str==out:
-        print('string is palindome.')
+        print('string is palindrome.')
     else:
-        print('string is not palindome.')
+        print('string is not palindrome.')
 
 
