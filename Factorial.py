a189ddaf545837a4f06472d500a4c8ec33d83bf2
Laxman Kumar <59361284+laxmankumar2000@users.noreply.github.com>
2020-05-19 15:55:11 +0530

    Factorial_number

---
+++ Factorial.py
@@ -0,0 +1,18 @@
+'''
+
+# calculates factorial of no 
+
+'''
+
+
+
+
+n = int(input())                # takes input from user in type integer
+fact = 1                            # a counter variable for calculation
+for i in range(1, n+1):
+    fact = fact*i
+print("factorial of", n, "is", fact)
+
+
+
+
